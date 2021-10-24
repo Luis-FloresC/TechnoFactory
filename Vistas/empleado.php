9fd4dc717740c985ba42a287f17c1378165fa9a6
javier8159 <mgerson815@gmail.com>
2021-10-24 17:38:23 +0200

    nm

--- Vistas/empleado.php
+++ Vistas/empleado.php
@@ -68,38 +68,6 @@ $fecha_actual=date("Y-m-d");
 
 
 
-
-<?php /*
-<h1>LISTADO DE EMPLEADOS</h1>
-<table width="70%">
-
-    <tr>
-        <td>nombre</td>
-        <td>apellido</td>
-        <td>direccion</td>
-        <td>Apellidos</td>
-        <td>fecha</td>
-        <td>genero</td>
-    </tr>
-    <?php 
-        while($datos=$resultado->fetch_array()){
-        ?>
-    <tr>
-        <td><?php echo $datos["idMedico"]?></td>
-        <td><?php echo $datos["medIdentificacion"]?></td>
-        <td><?php echo $datos["medNombres"]?></td>
-        <td><?php echo $datos["medApellidos"]?></td>
-        <td><?php echo $datos["medEspecialidad"]?></td>
-        <td><?php echo $datos["medTelefono"]?></td>
-        <td><?php echo $datos["medCorreo"]?></td>
-    </tr>
-  
-        }
-
-     ?>
-</table>
-
-
 <?php/*
 
 <! DOCTYPE html >

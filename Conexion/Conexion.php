e4c320bfd3caccd34aee47a41ea82eafb1f0b726
javier8159 <mgerson815@gmail.com>
2021-11-03 00:24:40 +0100

    cambio nombre a host en linea 4 de conexion

--- Conexion/Conexion.php
+++ Conexion/Conexion.php
@@ -1,7 +1,7 @@
 <?php 
 require_once "global.php";
 
-$conexion = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+$conexion = new mysqli($host,DB_USERNAME,DB_PASSWORD,DB_NAME);
 mysqli_query( $conexion, 'SET NAMES "'.DB_ENCODE.'"');
 
 //Si tenemos un posible error en la conexión lo mostramos
